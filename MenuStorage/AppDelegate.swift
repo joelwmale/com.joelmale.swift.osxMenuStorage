652f15ab71a03194cac02a1595bbf3a10422a50a
Joel Male <joel@joelmale.com>
2016-07-21 18:51:50 +1000

    Check frequency
    Started work on the ability to update the check frequency

--- MenuStorage/AppDelegate.swift
+++ MenuStorage/AppDelegate.swift
@@ -10,13 +10,25 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var updateTimer: NSTimer?
+    // Default
+    var updateFrequency : NSTimeInterval = 3.0;
 
     @IBAction func quitApplication(sender: NSMenuItem) {
         NSApplication.sharedApplication().terminate(self)
     }
-    
+
     @IBOutlet weak var statusMenu: NSMenu!
     @IBOutlet weak var window: NSWindow!
+    
+    @IBAction func update5Seconds(sender: NSMenuItem) {
+        updateTimer(5.0)
+    }
+    
+    @IBAction func update20Seconds(sender: NSMenuItem) {
+        updateTimer(20.0)
+    }
     
     // Get to the main status bar for the OS
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
@@ -29,7 +41,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let totalDiskSpace = convertBytesToGb(getTotalSize()!)
         
         // Set the title that the user will see
-        statusItem.title = "Free Disk Space: \(freeDiskSpace!)GB / \(totalDiskSpace!)GB"
+        //statusItem.title = "Free Disk Space: \(freeDiskSpace!)GB / \(totalDiskSpace!)GB"
+        statusItem.title = "My current update time is every \(updateFrequency) seconds"
+        
+        print("Updated menu")
+    }
+    
+    func updateTimer(time : NSTimeInterval) {
+        if (updateTimer != nil) {
+            updateTimer!.invalidate()
+        }
+        updateFrequency = time
+        updateTimer = NSTimer.scheduledTimerWithTimeInterval(time, target: self, selector: #selector(self.updateMenu), userInfo: nil, repeats: true)
     }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
@@ -41,7 +64,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         updateMenu()
         
         // Create a timer to call the update function
-        _ = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: #selector(self.updateMenu), userInfo: nil, repeats: true)
+        updateTimer(updateFrequency)
     }
 
 
